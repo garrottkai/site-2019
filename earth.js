40ae87bea67c810c76dc30afd643db49c77519b9
garrottkai <garrottkai@gmail.com>
2019-01-16 12:34:21 -0700

    Cleaning up mobile layout

--- earth.js
+++ earth.js
@@ -35,56 +35,56 @@ var geometry = new THREE.SphereGeometry(15, 48, 48);
 var mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh /*, lighting*/ );
 
-var getArray = (csv) => {
-  let lines = csv.split('\n');
-  lines.map(line => line.split(','));
-  return lines;
-};
+// var getArray = (csv) => {
+//   let lines = csv.split('\n');
+//   lines.map(line => line.split(','));
+//   return lines;
+// };
 
-var addData = (data) => {
-  //var dataGeometry = new THREE.Geometry();
-  var dataMaterial = new THREE.MeshBasicMaterial({
-    color: 0xFFFFFF
-  });
-  //var dataCylinder = new THREE.Mesh(new THREE.CylinderBufferGeometry(.1, .1, 16, 5, 5));
-  var dataCylinder = new THREE.CylinderBufferGeometry(.1, .1, 16, 5, 5);
-  var allData = [];
+// var addData = (data) => {
+//   //var dataGeometry = new THREE.Geometry();
+//   var dataMaterial = new THREE.MeshBasicMaterial({
+//     color: 0xFFFFFF
+//   });
+//   //var dataCylinder = new THREE.Mesh(new THREE.CylinderBufferGeometry(.1, .1, 16, 5, 5));
+//   var dataCylinder = new THREE.CylinderBufferGeometry(.1, .1, 16, 5, 5);
+//   var allData = [];
+//
+//   data.forEach(point => {
+//     let [lat, long, val] = point;
+//     let phi = (Math.PI / 180) * lat;
+//     let theta = (Math.PI / 180) * (long - 180);
+//
+//     let x = -16 * Math.cos(phi) * Math.cos(theta);
+//     let y = 16 * Math.sin(phi);
+//     let z = 16 * Math.cos(phi) * Math.sin(theta);
+//
+//     let dataVector = new THREE.Vector3(x, y, z);
+//     dataCylinder.position = dataVector;
+//     dataCylinder.lookAt(new THREE.Vector3(0, 0, 0));
+//     //dataGeometry.mergeMesh(dataCylinder);
+//     let dataMesh = new THREE.Mesh(dataCylinder, [new THREE.MeshBasicMaterial()]);
+//     allData.push(dataMesh);
+//   });
+//
+//   allData.forEach(item => {
+//     scene.add(item);
+//   })
+//
+// };
 
-  data.forEach(point => {
-    let [lat, long, val] = point;
-    let phi = (Math.PI / 180) * lat;
-    let theta = (Math.PI / 180) * (long - 180);
-
-    let x = -16 * Math.cos(phi) * Math.cos(theta);
-    let y = 16 * Math.sin(phi);
-    let z = 16 * Math.cos(phi) * Math.sin(theta);
-
-    let dataVector = new THREE.Vector3(x, y, z);
-    dataCylinder.position = dataVector;
-    dataCylinder.lookAt(new THREE.Vector3(0, 0, 0));
-    //dataGeometry.mergeMesh(dataCylinder);
-    let dataMesh = new THREE.Mesh(dataCylinder, [new THREE.MeshBasicMaterial()]);
-    allData.push(dataMesh);
-  });
-
-  allData.forEach(item => {
-    scene.add(item);
-  })
-
-};
-
-//$(document).ready(() => {
-jQuery.get('data/data.csv', data => {
-  data = getArray(data);
-  addData(data);
-});
-//});
+// //$(document).ready(() => {
+// jQuery.get('data/data.csv', data => {
+//   data = getArray(data);
+//   addData(data);
+// });
+// //});
 
 function animate() {
   requestAnimationFrame(animate);
 
   // rotate the model
-  mesh.rotation.y += 0.01;
+  mesh.rotation.y += 0.002;
 
   renderer.render(scene, camera);
 //  stats.update();
