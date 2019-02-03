df1300df127babd72210357a5c8d89ed457490da
garrottkai <garrottkai@gmail.com>
2019-02-02 21:12:40 -0700

    data processing works for 0.1deg source file

--- data/digestData.php
+++ data/digestData.php
@@ -13,13 +13,12 @@ require_once('fetchData.php');
 
 /*
 Parse the specifically formatted CSV data files used here.
-These files represent a 1 x 1 degree grid of the Earth's surface.
-Each line of the original CSV corresponds to an integer latitude value
-and consists of 360 decimal measurements, one for each integer longitude value.
-The return value of this function is a CSV string containing the nonzero measurements,
-one per line, in the format: latitude,longitude,measurement
+These files represent a 0.1 x 0.1 degree grid of the Earth's surface.
+Each of the 1800 lines of the original CSV corresponds to an 0.1 degree latitude value
+and consists of 3600 decimal measurements, one for each 0.1 degree longitude value.
+The return value of this function is a CSV string containing only the nonzero measurements falling on integer degree values, one per line, in the format: latitude,longitude,measurement
 */
-function parseFile(string $fileName, $date): string {
+function parseFile(string $fileName, string $date): string {
 
     $file = file_get_contents($fileName);
 
@@ -27,37 +26,56 @@ function parseFile(string $fileName, $date): string {
     $lines = preg_split('/\r\n|\r|\n/', $file);
 
     // output to serialize; begins with IS0 date of the data
-    $processed = [$date];
+    $processed = [$date . "\n"];
 
     // move from north to south through the array
     $lat = -90;
 
-    foreach($lines as $line) {
+    $numLines = count($lines);
+
+    for($i = 0; $i < $numLines; $i += 10) {
+
         // move from west to east on the line
+        $line = $lines[$i];
         $long = -180;
         $procLine = [];
         $values = explode(',', $line);
-        foreach($values as $value) {
+
+        $numPoints = count($values);
+
+        for($j = 0; $j < $numPoints; $j += 10) {
+
+            $value = $values[$j];
             // 99999.0 is the zero value; we drop these but increment the long counter
             if($value && $value != 99999.0) {
+
                 $coords = $lat . "," . $long . "," . $value . "\n";
                 // add the new coordinate line to the output
                 array_push($procLine, $coords);
+
             }
-            $long += 1;
+
+            ++$long;
+
         }
+
         // dump the values for the line in with the rest - no need to preserve original format
         $processed = array_merge($processed, $procLine);
-        $lat += 1;
+        ++$lat;
+
     }
+
     return(implode($processed));
+
 }
 
 // create the output CSV file
 function writeFile(string $data): void {
+
     $file = fopen('data.csv', 'w');
     fwrite($file, $data);
     fclose($file);
+
 }
 
 try {
@@ -72,7 +90,7 @@ try {
 
     } else {
 
-        echo 'No new data found.';
+        echo "No new data found.\n";
 
     }
 
